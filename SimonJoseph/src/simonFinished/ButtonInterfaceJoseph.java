6d20fae3cd5d100e0b7883574172c70db028d235
TheJoeJoe <jhur4626@bths.edu>
2017-01-07 22:27:55 -0500

    Update ButtonInterfaceJoseph.java

--- SimonJoseph/src/simonFinished/ButtonInterfaceJoseph.java
+++ SimonJoseph/src/simonFinished/ButtonInterfaceJoseph.java
@@ -1,7 +1,21 @@
 package simonFinished;
 
+import java.awt.Color;
+
+import GUIpractice.components.Action;
 import GUIpractice.components.Clickable;
 
 public interface ButtonInterfaceJoseph extends Clickable {
+	
+	void setAction(Action action);
 
+	void highlight();
+
+	void dim();
+
+	void setY(int i);
+
+	void setX(int i);
+
+	void setColor(Color color);
 }
